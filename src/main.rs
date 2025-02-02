48efe4020529b9cdf16ac281aa0f589021845a89
Ilias Stamatis <ilstam@mailbox.org>
2025-02-02 11:29:52 +0000

    Makefile.toml: Add clippy and fmt targets

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,13 @@ use core::panic::PanicInfo;
 global_asm!(include_str!("boot.s"));
 
 #[panic_handler]
+#[allow(clippy::empty_loop)]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
 #[no_mangle]
+#[allow(clippy::empty_loop)]
 pub fn main() -> ! {
     loop {}
 }
