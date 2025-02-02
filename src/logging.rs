7a796dc378a4af1bbc80b94941f8203a8d2c7c17
Ilias Stamatis <ilstam@mailbox.org>
2025-02-02 19:47:56 +0000

    Print a message upon panicking and dump the registers
    Also move the panic handler to logging.rs
    
    The register dump is not complete yet. The handler will print something
    like that:
    
    KERNEL PANIC - Test panic ('src/main.rs', line 16, column 5)
    
    Register dump:
    x0: 0x0000000000000000 x1: 0x0000000000000000 x2: 0x0000000000000000 x3: 0x00000000000807ec
    fp: 0x000000000007fe10 lr: 0x000000000008117c sp: 0x000000000007f2e0

--- src/logging.rs
+++ src/logging.rs
@@ -1,3 +1,7 @@
+use core::arch::asm;
+use core::panic::PanicInfo;
+use core::sync::atomic::{AtomicBool, Ordering};
+
 pub struct SerialConsole;
 
 impl core::fmt::Write for SerialConsole {
@@ -27,4 +31,69 @@ macro_rules! println {
         print!($($arg)*);
         print!("\n");
     }};
+}
+
+fn dump_registers() {
+    let x0: u64;
+    let x1: u64;
+    let x2: u64;
+    let x3: u64;
+    let fp: u64;
+    let lr: u64;
+    let sp: u64;
+
+    // SAFETY: Simply moving registers and then copying them to memory
+    unsafe {
+        asm!(
+            "mov {0}, x0",
+            "mov {1}, x1",
+            "mov {2}, x2",
+            "mov {3}, x3",
+            "mov {4}, fp",
+            "mov {5}, lr",
+            "mov {6}, sp",
+            out(reg) x0,
+            out(reg) x1,
+            out(reg) x2,
+            out(reg) x3,
+            out(reg) fp,
+            out(reg) lr,
+            out(reg) sp,
+        );
+    }
+
+    println!(
+        "Register dump:\n\
+        x0: 0x{:016x} x1: 0x{:016x} x2: 0x{:016x} x3: 0x{:016x}\n\
+        fp: 0x{:016x} lr: 0x{:016x} sp: 0x{:016x}",
+        x0, x1, x2, x3, fp, lr, sp
+    );
+}
+
+#[panic_handler]
+#[cfg(not(test))]
+pub fn panic(info: &PanicInfo) -> ! {
+    // A nested panic could occur while we try to print our panic message.
+    // In that case do not attempt to print anything and just loop forever.
+    static IN_PROGRESS: AtomicBool = AtomicBool::new(false);
+    if IN_PROGRESS.swap(true, Ordering::Relaxed) {
+        loop {}
+    }
+
+    let (file, line, column) = match info.location() {
+        Some(location) => (location.file(), location.line(), location.column()),
+        _ => ("", 0, 0),
+    };
+
+    println!(
+        "\nKERNEL PANIC - {} ('{}', line {}, column {})\n",
+        info.message(),
+        file,
+        line,
+        column,
+    );
+
+    dump_registers();
+
+    loop {}
 }
--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,14 @@
 mod logging;
 
 use core::arch::global_asm;
-use core::panic::PanicInfo;
 
 global_asm!(include_str!("boot.s"));
 
-#[panic_handler]
-#[allow(clippy::empty_loop)]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
 #[no_mangle]
-#[allow(clippy::empty_loop)]
 pub fn main() -> ! {
     let a = 4;
     let b = 5;
     println!("Hello {} with some math: {a} + {b} = {}", "world", a + b);
 
-    loop {}
+    panic!("Test panic");
 }
